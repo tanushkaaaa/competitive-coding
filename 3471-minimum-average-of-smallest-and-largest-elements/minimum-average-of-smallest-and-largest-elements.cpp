2adf63aa5ddf632a329b98766c23c50f66e55ec2
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2024-06-26 22:13:19 +0530

    Time: 8 ms (34.93%) | Memory: 24.9 MB (70.52%) - LeetSync

--- 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
@@ -4,7 +4,7 @@ public:
         int n=nums.size();
         sort(nums.begin(),nums.end());
         int i=0, j=n-1;
-        double ans=100;
+        double ans=INT_MAX;
         while(i<j){
             double k=(nums[i]+nums[j])/2.0;
             ans=min(ans,k);
