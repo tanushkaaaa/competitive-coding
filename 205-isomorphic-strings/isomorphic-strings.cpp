707eca900cd27ce156ba90f87f18a754a29939f1
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2024-08-07 16:23:09 +0530

    Time: 7 ms (44.58%) | Memory: 9.4 MB (35.80%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -9,11 +9,14 @@ public:
             char ch1=s[i];
             char ch2=t[i];
 
+            
+
         if(mp1.find(ch1)!=mp1.end()&& mp1[ch1]!=ch2 || mp2.find(ch2)!=mp2.end() && mp2[ch2] !=ch1){
             return false;
         }
         mp1[ch1]=ch2;
         mp2[ch2]=ch1;
+        
         }
         return true;
     }
