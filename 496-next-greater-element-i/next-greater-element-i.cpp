1a2c156da1bab99c60459d793e3dfe2f7b9f1a31
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2024-08-08 08:46:16 +0530

    Time: 8 ms (31.09%) | Memory: 12.7 MB (5.18%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,19 +1,33 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>res(nums1.size() , -1);
-        unordered_map<int,int>map;
-        stack<int>st;
-        for(int num:nums2){
-            while(!st.empty() && st.top()<num){
-                map[st.top()]=num;
+    vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
+    {
+        vector<int> output;
+        unordered_map<int, int> hashMap;
+
+        for (int num : nums1)
+        {
+            hashMap[num] = -1;
+        }
+
+        stack<int> st;
+
+        for (int num : nums2)
+        {
+            while (!st.empty() && st.top() < num)
+            {
+                hashMap[st.top()] = num;
                 st.pop();
             }
             st.push(num);
         }
-        for(int i=0;i<nums1.size();i++){
-            if(map[nums1[i]]) res[i]=map[nums1[i]];
+
+        for (int num : nums1)
+        {
+            output.push_back(hashMap[num]);
         }
-        return res;
+
+        return output;
     }
 };
