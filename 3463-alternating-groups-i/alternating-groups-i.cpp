d7c88000194db55a5b9a66d038b924592d16f5a8
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2024-08-09 17:43:28 +0530

    Time: 10 ms (19.59%) | Memory: 26.5 MB (9.75%) - LeetSync

--- 3463-alternating-groups-i/alternating-groups-i.cpp
+++ 3463-alternating-groups-i/alternating-groups-i.cpp
@@ -3,11 +3,12 @@ public:
     int numberOfAlternatingGroups(vector<int>& colors) {
         int n=colors.size();
         int c=0;
+         if((colors[n-1]!=colors[n-2]) && (colors[n-1]!=colors[0])) c++;
+        if((colors[n-1]!=colors[0]) && (colors[0]!=colors[1])) c++;
         for(int i=1;i<n-1;i++){
         if( (colors[i]!=colors[i+1]) && (colors[i]!=colors[i-1])) c++;
         }
-        if((colors[n-1]!=colors[n-2]) && (colors[n-1]!=colors[0])) c++;
-        if((colors[n-1]!=colors[0]) && (colors[0]!=colors[1])) c++;
+       
         return c;
     }
 };
