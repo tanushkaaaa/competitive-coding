59361e44542e12775ead87d0db4e2bee0db5dd8c
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2024-08-24 22:43:20 +0530

    Time: 6 ms (32.18%) | Memory: 12 MB (34.64%) - LeetSync

---
+++ 682-baseball-game/baseball-game.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<int> a;
+        
+        for (const string& op : operations) {
+            if (op == "+") {
+                a.push_back(a[a.size() - 1] + a[a.size() - 2]);
+            } 
+            else if (op == "D") 
+            {
+                a.push_back(2 * a.back());
+            } 
+            else if (op == "C") {
+                a.pop_back();
+            } 
+            else {
+                a.push_back(stoi(op));
+            }
+        } 
+        return accumulate(a.begin(), a.end(), 0);
+    }
+};
