4b4ee5d86f2e2b595a5c6a271001b7f7624e0aa8
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2024-09-03 12:02:24 +0530

    Time: 10 ms (45.83%) | Memory: 10 MB (28.49%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()){return false;}
+        if(s.length()!=t.length()) return false;
         unordered_map<char,int> m;
-        for(char ch:s){ m[ch]++;}
-        for(char ch:t){
-            if(m.find(ch)==m.end()|| m[ch]==0){
-                return false;
-            }
-            m[ch]--;
+        for(auto ch:s) {m[ch]++;}
+        for(auto ch:t){
+            if(m.find(ch)==m.end()||m[ch]==0) return false;
+        
+        m[ch]--;
         }
         return true;
     }
