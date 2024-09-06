d0cc8fbeaeced3dd5933d614d281372a42491782
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2024-09-06 22:02:40 +0530

    Time: 2095 ms (16.44%) | Memory: 768.7 MB (12.10%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n<=1) return n;
+        vector<int>dp(n+1,-1);
+        if(dp[n]!=-1) return dp[n];
+        dp[n]=fib(n-1) + fib(n-2);
+        return dp[n];
+    }
+};
