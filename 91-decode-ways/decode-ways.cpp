de4e12eebf223d029a50f0fc2fb725f382e4c8e3
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2024-09-13 08:19:05 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (44.68%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        if(s.empty()|| s[0]=='0') return 0;
+        int n=s.length();
+        vector<int>dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            int od=s[i-1]-'0';
+            int td=stoi(s.substr(i-2,2));
+
+            if(od!=0) dp[i]+=dp[i-1];
+            if(10<=td && td<=26) dp[i]+=dp[i-2];
+        }
+        return dp[n];
+    }
+};
