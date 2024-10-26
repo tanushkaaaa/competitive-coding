7c1345dbc3e8a217c81639859351243bc4eb7be5
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2024-10-26 15:24:41 +0530

    Time: 59 ms (95.04%) | Memory: 76.3 MB (77.95%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_set<int> window;
-    
-    for (int i = 0; i < nums.size(); ++i) {
-        // If we find the current element in the set, we have a duplicate
-        if (window.find(nums[i]) != window.end()) {
-            return true;
+       unordered_set<int> s;
+       for(int i=0;i<nums.size();i++){
+        if(s.find(nums[i])!=s.end())  return true;
+       
+
+        s.insert(nums[i]);
+
+        if(s.size()>k){
+            s.erase(nums[i-k]);
         }
-        
-        // Add current element to the set
-        window.insert(nums[i]);
-        
-        // If window size exceeds k, remove the oldest element
-        if (window.size() > k) {
-            window.erase(nums[i - k]);
-        }
-    }
-    
-    return false;
+       }
+       return false;
     }
 };
