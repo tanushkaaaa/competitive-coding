deb888b7e92dfcc03ae6c86db177a5375488a90b
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2024-12-05 17:30:09 +0530

    Time: 48 ms (24.20%) | Memory: 75.8 MB (9.05%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int>m;
-        for(auto& i:nums) m[i]++;
-        for(auto& p:m) {
+        unordered_map<int,int> m;
+        for(auto i:nums){
+            m[i]++;
+        }
+        for(auto p:m){
             if(p.second>1) return true;
-           // break;
-        } 
+        }
         return false;
     }
-};
+    };
