1b180acb074e31a05f10f665a645148085948ce4
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2024-12-05 19:43:04 +0530

    Time: 6 ms (42.10%) | Memory: 9.9 MB (36.97%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       if(s.length()!=t.length()) return false;
-       unordered_map<char,int> m;
-       for(auto ch:s) {m[ch]++;}
-       for(auto ch: t){
-        if(m.find(ch)==m.end() || m[ch]==0) {return false; break;}
-       m[ch]--;
-        }
-        return true;
+      if(s.length()!=t.length()) return false;
+      unordered_map<char,int> m;
+      for(auto ch: s) {m[ch]++;}
+      for(auto ch:t){
+            if(m.find(ch)==m.end()||m[ch]==0){ return false;
+            break;}
+            m[ch]--;
+      
+      }
+      return true;
     }
+
 };
