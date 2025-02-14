d7a62d6d6b37019a188f9729caf592b820a92539
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2025-02-14 10:54:19 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (70.43%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
     int maximum69Number(int num) {
-        string numStr = to_string(num);
-        for (char &ch : numStr) { 
-            if (ch == '6') { 
-                ch = '9';
-                break; // Change only the first occurrence
-            }
+        //greedy
+        int placeval = 0 ;
+        int tem = num;
+        int sixPlaceval = -1; 
+        while (tem > 0){
+            if (tem % 10 == 6){
+                sixPlaceval = placeval;}
+            tem = tem/10;
+            placeval += 1;
         }
-        return stoi(numStr);
+        if(sixPlaceval==-1) return num;
+
+        return num+3*pow(10,sixPlaceval);
     }
 };
+// string numStr = to_string(num);
+//         for (char &ch : numStr) { 
+//             if (ch == '6') { 
+//                 ch = '9';
+//                 break; // Change only the first occurrence
+//             }
+//         }
+//         return stoi(numStr);
