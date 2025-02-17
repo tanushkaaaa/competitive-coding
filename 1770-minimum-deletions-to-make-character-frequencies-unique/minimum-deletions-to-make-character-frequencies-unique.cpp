3a3ade64646c1e1366c54cbca6c6e73069625f8c
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2025-02-17 10:19:55 +0530

    Time: 3 ms (80.57%) | Memory: 20.2 MB (87.04%) - LeetSync

--- 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -7,7 +7,10 @@ public:
         }
         sort(begin(freq),end(freq));
         int res=0;
-        for(int i=24;i>=0 && freq[i]>0; i--){
+        for(int i=24;i>=0; i--){
+            if (freq[i] == 0) {
+                break;
+            }
             if(freq[i]>=freq[i+1]){
                 int prev=freq[i];
                 freq[i]=max(0,freq[i+1]-1);//taki -ve na jaaye
