0c433096488ff867ae2cd421a60457cbebbee443
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2025-02-20 17:21:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (15.81%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char>st;
+        int c=0;
+        for(char ch:s){
+            if(ch=='(') st.push(ch);
+            else{
+                if(st.empty()) c++;
+                else st.pop();
+            }
+        }
+        return c+st.size();
+    }
+};
