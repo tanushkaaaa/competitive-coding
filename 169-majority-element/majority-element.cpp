a6762b80a35d5c1a09c054ae741bc517d0788c84
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2025-03-12 21:52:22 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (84.39%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,8 +1,32 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n=nums.size();
-       sort(nums.begin(),nums.end());
-       return nums[n/2];
+      int n=nums.size();
+    //    sort(nums.begin(),nums.end());
+    //    return nums[n/2];
+    //but soting takes tc O(n logn)
+
+    // unordered_map<int,int>m;
+    // for(int i:nums){
+    //     m[i]++;
+    // }
+    // for(auto p:m){
+    //     if(p.second>n/2) return p.first;
+    // }
+    // return 0;
+    //but map takes sc O(n)
+
+    //moores vooting algo
+    int c=0;
+    int el;
+    for(int i=0;i<n;i++){
+        if(c==0){
+            c=1;
+            el=nums[i];
+        }
+        else if(nums[i]==el) c++;
+        else c--;
+    }
+    return el;
     }
 };
