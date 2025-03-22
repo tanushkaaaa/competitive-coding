38f38e4c7ecbf5278a370d001ca92e2e5b6441cb
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2025-03-22 16:15:21 +0530

    Time: 4 ms (72.77%) | Memory: 38.2 MB (83.64%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char,int>m;
-        int c=0;
-        for(auto i:tasks){
-            m[i]++;
-            c=max(c,m[i]);
-        }
-        int ans=(c-1)*(n+1);
-        for(auto p:m){
-            if(p.second==c) ans++;
-        }
+       unordered_map<char,int> m;
+       int c=0;
+       for(char ch:tasks){
+        m[ch]++;
+        c=max(c,m[ch]);
+       }
+       int ans=(c-1)*(n+1);
+       for(auto p:m){
+        if(p.second==c) ans++;
+       }
         return max((int)tasks.size(),ans);
     }
 };
