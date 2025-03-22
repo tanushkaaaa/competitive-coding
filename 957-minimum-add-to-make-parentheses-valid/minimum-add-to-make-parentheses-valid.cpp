288ce00762e7700e44e1093fc0b4d880455df9b2
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2025-03-22 15:30:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (54.82%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-       stack<char>st;
-       int c=0;
-       for(char ch:s){
-        if(ch=='(') st.push(ch);
+    //    stack<char>st;
+    //    int c=0;
+    //    for(char ch:s){
+    //     if(ch=='(') st.push(ch);
+    //     else{
+    //         if(st.empty()) c++;
+    //         else st.pop();
+    //     }
+    //    }
+    //    return c+st.size();
+    int open=0,close=0;
+    for(char ch:s){
+        if(ch=='(') open++;
         else{
-            if(st.empty()) c++;
-            else st.pop();
+            if(open>0) open--;
+            else close++;
         }
-       }
-       return c+st.size();
+    }
+    return open+close;
     }
 };
