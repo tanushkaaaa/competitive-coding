cabc30a12ae7e58e50f32599b4385c5f2fdc1316
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2025-03-31 13:58:28 +0530

    Time: 4 ms (56.22%) | Memory: 99.8 MB (86.95%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
@@ -11,26 +11,41 @@ public:
         // }
         // return time;
         //this wont work ex in case of [3,5,10,7,5,3,5,5,4,8,1]
-         int time = 0;
-        int maxTime = neededTime[0];  // Track the max time in the current group
-        int totalTime = neededTime[0];  // Sum of all times in the current group
 
-        for (int i = 1; i < colors.size(); i++) {
-            if (colors[i] == colors[i - 1]) { 
-                // Same color group
-                totalTime += neededTime[i];
-                maxTime = max(maxTime, neededTime[i]);
-            } else {
-                // Add the cost to remove all but the max balloon in the previous group
-                time += (totalTime - maxTime);
-                // Reset for the new group
-                totalTime = neededTime[i];
-                maxTime = neededTime[i];
+
+
+        //  int time = 0;
+        // int maxTime = neededTime[0];  // Track the max time in the current group
+        // int totalTime = neededTime[0];  // Sum of all times in the current group
+
+        // for (int i = 1; i < colors.size(); i++) {
+        //     if (colors[i] == colors[i - 1]) { 
+        //         // Same color group
+        //         totalTime += neededTime[i];
+        //         maxTime = max(maxTime, neededTime[i]);
+        //     } else {
+        //         // Add the cost to remove all but the max balloon in the previous group
+        //         time += (totalTime - maxTime);
+        //         // Reset for the new group
+        //         totalTime = neededTime[i];
+        //         maxTime = neededTime[i];
+        //     }
+        // }
+
+        // // Handle the last group
+        // time += (totalTime - maxTime);
+        // return time;
+        //this will work but this is gadha majduri
+
+        //this is smart soln
+        int TotalTime=0;
+        for(int i=1;i<colors.size();i++){
+            if(colors[i]==colors[i-1]){
+            TotalTime+=min(neededTime[i],neededTime[i-1]);
+            neededTime[i]=max(neededTime[i],neededTime[i-1]);//-->Point**
             }
+            
         }
-
-        // Handle the last group
-        time += (totalTime - maxTime);
-        return time;
+        return TotalTime;
     }
 };
