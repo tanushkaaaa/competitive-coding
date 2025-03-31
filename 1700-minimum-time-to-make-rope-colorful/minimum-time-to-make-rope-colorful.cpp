a404ee3e1b4a9bdea4c1be651a23125fc922cee8
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2025-03-31 13:45:36 +0530

    Time: 7 ms (32.99%) | Memory: 99.8 MB (63.31%) - LeetSync

---
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+        // int time=0;
+        // int n=colors.length();
+        // for(int i=1;i<n;i++){
+        //         if(colors[i-1]==colors[i]){
+        //             if(neededTime[i]>neededTime[i-1]) time+=neededTime[i-1];
+        //             else time+=neededTime[i];
+        //         }
+        // }
+        // return time;
+        //this wont work ex in case of [3,5,10,7,5,3,5,5,4,8,1]
+         int time = 0;
+        int maxTime = neededTime[0];  // Track the max time in the current group
+        int totalTime = neededTime[0];  // Sum of all times in the current group
+
+        for (int i = 1; i < colors.size(); i++) {
+            if (colors[i] == colors[i - 1]) { 
+                // Same color group
+                totalTime += neededTime[i];
+                maxTime = max(maxTime, neededTime[i]);
+            } else {
+                // Add the cost to remove all but the max balloon in the previous group
+                time += (totalTime - maxTime);
+                // Reset for the new group
+                totalTime = neededTime[i];
+                maxTime = neededTime[i];
+            }
+        }
+
+        // Handle the last group
+        time += (totalTime - maxTime);
+        return time;
+    }
+};
