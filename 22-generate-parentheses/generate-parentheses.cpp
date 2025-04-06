9c7260fd387f31e7042b8e7a6e825a99a00dbc6d
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2025-04-06 11:18:53 +0530

    Time: 1 ms (86.94%) | Memory: 13.2 MB (78.88%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+//backTracking
+
+    vector<string>res;
+    void solve(string &curr,int n,int open,int close){
+        if(curr.length()==2*n){
+            res.push_back(curr);
+            return;
+        }
+        if(open<n){
+        curr.push_back('(');
+        solve(curr,n,open+1,close);
+        curr.pop_back();
+        }
+        if(close<open){
+          curr.push_back(')');
+        solve(curr,n,open,close+1);
+        curr.pop_back();  
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        string curr="";
+        int open=0,close=0;
+        solve(curr,n,open,close);
+        return res;
+    }
+};
