e0c3b5d843164d0ee5b805bae058df5299794daa
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2025-04-06 10:51:08 +0530

    Time: 21 ms (42.16%) | Memory: 28.3 MB (83.74%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
     string removeStars(string s) {
-        stack<char>st;
-        for(int i=0;i<s.length();i++){
-            if(!st.empty() && s[i]=='*') st.pop();
-            else st.push(s[i]);
-        }
-        string result;
-        while (!st.empty()) {
-            result += st.top();
-            st.pop();
-        }
+        // stack<char>st;
+        // for(int i=0;i<s.length();i++){
+        //     if(!st.empty() && s[i]=='*') st.pop();
+        //     else st.push(s[i]);
+        // }
+        // string result;
+        // while (!st.empty()) {
+        //     result += st.top();
+        //     st.pop();
+        // }
 
-        // Stack gives characters in reverse order
-        reverse(result.begin(), result.end());
+        // // Stack gives characters in reverse order
+        // reverse(result.begin(), result.end());
 
-        return result;
+        // return result;
+        //ye sab stack se krne ka tarika h but direct vector use kr skte chota krne k liye 
+         string c="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='*'){
+                c.pop_back();
+            }else{
+                c+=s[i];
+            }
+            
+        }
+        return c;
     }
 };
