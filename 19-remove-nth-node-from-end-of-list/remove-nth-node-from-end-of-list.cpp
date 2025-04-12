c3e18c9d02e0781df32cb0c0b470ea1cba84131f
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2025-04-12 16:59:27 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (25.49%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -11,32 +11,17 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        // ListNode* slow=head;
-        // ListNode* fast=head;
-        // for(int i=0; i<n;i++){
-        //     fast=fast->next;
-        // }
-        // if(fast==NULL) return head->next;
-        // while(fast->next!=NULL){
-        //     fast=fast->next;
-        //     slow=slow->next;
-        // }
-        
-        // slow->next=slow->next->next;
-        
-        // return head;
-        int len=1;
-        ListNode* tail=head;
-        while(tail->next){
-            tail=tail->next;
-            len++;
-        }
-        if(len==n) return head->next;
-        tail=head;
-        for(int i=0;i<len-n-1;i++){
-            tail=tail->next;
-        }
-         tail->next = tail->next->next;
-          return head;
+       ListNode* fast=head;
+       ListNode* slow=head;
+       for(int i=0;i<n;i++){
+        fast=fast->next;
+       }
+       if(fast==NULL) return head->next;
+       while(fast->next!=NULL){
+        fast=fast->next;
+        slow=slow->next;
+       }
+       slow->next=slow->next->next;
+       return head;
     }
 };
