b48a7a5e41f633b5020558c4a272a9d5c1bed003
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2025-04-13 23:17:46 +0530

    Time: 7 ms (68.54%) | Memory: 11.9 MB (49.45%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -7,11 +7,11 @@ public:
 
         for(int r=0;r<s.length();r++){
             char c=s[r];
-            cnt[c]=cnt[c]+1;
-
+            cnt[s[r]]++;
             while(cnt[c]>1){
                 char leftChar= s[l];
-                cnt[leftChar]= cnt[leftChar]-1;
+                // cnt[leftChar]= cnt[leftChar]-1;
+                cnt[leftChar]--;
                 l++;
             }
             maxL=max(maxL,r-l+1);
