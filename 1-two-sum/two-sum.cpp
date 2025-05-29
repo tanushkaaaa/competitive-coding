c3f3b7208e23ad7bd88c44f3fc04ce4a1510ae4a
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2025-05-29 22:51:30 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (21.67%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-      unordered_map<int,int> m;
-      for(int i=0;i<nums.size();i++){
+    unordered_map<int,int>m;
+    for(int i=0;i<nums.size();i++){
         int comp=target-nums[i];
         if(m.count(comp)==1) return{i,m[comp]};
-        m[nums[i]] = i;
-      }
-      return{};
+        m[nums[i]]=i;
+    }
+        return {}; 
+    
+      //we used hashmap kyuki sc bhadra pr tc kam h, niche m while bhi lagana hoga toh tc bhad jayga
+    // int i=0;
+    // for(int j=i+1;j<nums.size();j++){
+    //     int k=nums[i];
+    //     if(target-nums[j]==k){
+    //         return {i,j};
+    //     }
+    //     else i++;
+    // }
+    // return {};
     }
 };
