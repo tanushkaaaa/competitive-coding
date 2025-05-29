a4e276daaaffb5b58672e5ca21b9068bb132481c
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2025-05-29 22:17:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (35.50%) - LeetSync

---
+++ 319-bulb-switcher/bulb-switcher.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        //perfect square wale on rhege
+        //brute force for loop from 1 to number of bulbs and check and c++ for every perfect square
+        //optimized approach 
+        return sqrt(n);
+        //ese questions ko examples solve kr k pattern dekh k solve krna chaiye
+        
+    }
+};
