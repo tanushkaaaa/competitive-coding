81ee079394a41d2c1ae1a8d42c130e476ab2bf4d
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2025-06-04 00:04:16 +0530

    Time: 32 ms (56.11%) | Memory: 61.5 MB (58.69%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -2,8 +2,16 @@ class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         //sort kr k krne ka TC is nlogn that is easy also
-       sort(begin(nums),end(nums),greater<int>());
-       return nums[k-1]; 
-        
+    //    sort(begin(nums),end(nums),greater<int>());
+    //    return nums[k-1]; 
+
+    //next approach is min heap iska tc is nlogk but in worst case nlogn toh similar hi hogya that is why this is not the best approach
+    // priority_queue<int,vector<int>>pq; this is default max heap and we need minheap
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(int num:nums){
+            pq.push(num);
+            if(pq.size()>k) pq.pop();
+        }
+        return pq.top();
     }
 };
