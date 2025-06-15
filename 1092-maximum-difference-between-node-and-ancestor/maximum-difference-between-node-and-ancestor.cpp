6ec62d712a145995d87fb2c2f46eecc9e1266bd7
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2025-06-15 23:46:40 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (60.89%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.cpp
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.cpp
@@ -11,23 +11,39 @@
  */
 class Solution {
 public:
- int maxDiff;
-    void maxDiffC(TreeNode* root,TreeNode* child){
-        if(!root || !child) return;
-        maxDiff= max(maxDiff,abs(root->val - child->val));
-        maxDiffC(root,child->left);
-         maxDiffC(root,child->right);
-    }
-    void maxDiffA(TreeNode* root){
-        if(!root) return;
-        maxDiffC(root,root->left);
-        maxDiffC(root,root->right);
-        maxDiffA(root->left);
-        maxDiffA(root->right);
-    }
-    int maxAncestorDiff(TreeNode* root) {
-         maxDiff=-1;
-        maxDiffA(root);
-        return maxDiff;
-    }
+//  int maxDiff;
+//     void maxDiffC(TreeNode* root,TreeNode* child){
+//         if(!root || !child) return;
+//         maxDiff= max(maxDiff,abs(root->val - child->val));
+//         maxDiffC(root,child->left);
+//          maxDiffC(root,child->right);
+//     }
+//     void maxDiffA(TreeNode* root){
+//         if(!root) return;
+//         maxDiffC(root,root->left);
+//         maxDiffC(root,root->right);
+//         maxDiffA(root->left);
+//         maxDiffA(root->right);
+//     }
+//     int maxAncestorDiff(TreeNode* root) {
+//          maxDiff=-1;
+//         maxDiffA(root);
+//         return maxDiff;
+//    }
+
+int findDiff(TreeNode* root,int maxV,int minV){
+    if(!root) return abs(minV-maxV);
+
+    minV=min(minV,root->val);
+    maxV=max(maxV,root->val);
+
+    int l= findDiff(root->left,maxV,minV);
+    int r= findDiff(root->right,maxV,minV);
+
+    return max(l,r);
+}
+int maxAncestorDiff(TreeNode* root) {
+    return findDiff(root,root->val,root->val);
+}
+
 };
