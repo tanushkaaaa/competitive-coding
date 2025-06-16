7ee44096f74c2b3794c90c96ef3af05d7501ec3e
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2025-06-17 00:15:06 +0530

    Time: 20 ms (94.40%) | Memory: 16.2 MB (88.85%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -3,7 +3,6 @@ public:
     int m,n;
     void dfs(vector<vector<char>>& grid,int i,int j){
         if(i<0 || i>=m || j<0 || j>=n || grid[i][j]!='1') return;
-        //if(grid[i][j]==-1) return;
         grid[i][j]='$';
         dfs(grid,i+1,j);
         dfs(grid,i-1,j);
