8061ac7db2d7d4e83eb30a7cc11471075b7e5c5c
tanushkaaaa <115868656+tanushkaaaa@users.noreply.github.com>
2025-06-22 22:39:44 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (27.03%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,5 +1,16 @@
 class Solution {
 public:
+//method-1 hashmap
+//method 2 sorting
+    // int singleNumber(vector<int>& nums) { 
+    //    sort(nums.begin(),nums.end());
+    //     for(int i=1;i<nums.size();i+=2)
+    //     {
+    //         if(nums[i]!=nums[i-1])
+    //             return nums[i-1];
+    //     }
+    //     return nums[nums.size()-1];
+//method3-bitwise
     int singleNumber(vector<int>& nums) {
          if(nums.size()==1)
         {
@@ -10,8 +21,6 @@ public:
         //for(int i=0;i<nums.size();i++){
             ans=ans^num;
         }
-        return ans;
-
-        
+        return ans;        
     }
 };
